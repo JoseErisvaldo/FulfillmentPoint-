56959f57c79c25c58e33c19cbc97f235b8bedb17
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-22 21:15:25 -0300

    Feat: home & NavBar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <ul>
+          <li>Notificações</li>
+          <li>Conta</li>
+          <li>Sair</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import NavBar from './Components/NavBar/NavBar'
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
