df8d3a530dd3d82f88e5a2c57344d8671156d9ac
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-24 21:21:33 -0300

    Feat: icon Btn

--- src/Components/Ux/Button/Button.jsx
+++ src/Components/Ux/Button/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function Button({ title, onClick, submit }) {
+export default function Button({ title, onClick, submit, icon }) {
   return (
     <div
       onClick={onClick}
       submit={submit}
       className="flex items-center gap-2 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 cursor-pointer"
     >
-      {title}
+      <span className="text-white text-2xl">{icon}</span>
+      <span>{title}</span>
     </div>
   )
 }
