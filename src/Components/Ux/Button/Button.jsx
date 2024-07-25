3bd3ce2f753ab94c616cb3d5e4c35079a71f4fee
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-24 21:03:35 -0300

    Feat: add Onclick

--- src/Components/Ux/Button/Button.jsx
+++ src/Components/Ux/Button/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function Button({ title }) {
+export default function Button({ title, onClick }) {
   return (
-    <div className="flex items-center gap-2 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 cursor-pointer">
+    <div
+      onClick={onClick}
+      className="flex items-center gap-2 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 cursor-pointer"
+    >
       {title}
     </div>
   )
