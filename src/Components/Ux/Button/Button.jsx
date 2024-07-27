b2bb51327656faa20960cc58ba58ac78ff54d27b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-26 22:06:37 -0300

    style: bg, color

--- src/Components/Ux/Button/Button.jsx
+++ src/Components/Ux/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Button({ title, onClick, type, icon }) {
+export default function Button({ title, onClick, type, icon, bg, color }) {
   return (
     <div
       onClick={onClick}
       type={type}
-      className="flex items-center gap-2 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 cursor-pointer"
+      className={`flex items-center gap-2 bg-${bg}-600 hover:bg-${bg}-500 text-${color} font-bold py-2 px-4 rounded mt-4 cursor-pointer`}
     >
       <span className="text-white text-2xl">{icon}</span>
       <span>{title}</span>
