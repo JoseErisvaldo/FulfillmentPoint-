dc09999c6a780da2cf402997627afa45e20b0b1d
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-27 15:09:45 -0300

    style: is modal

--- src/Components/Ux/Modal/Modal.jsx
+++ src/Components/Ux/Modal/Modal.jsx
@@ -7,7 +7,11 @@ export default function Modal({ isOpen, onClose, title, children }) {
   if (!isOpen) return null
 
   return ReactDOM.createPortal(
-    <div className=" is-open fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ${
+        isOpen ? 'is-open' : ''
+      }`}
+    >
       <div className="bg-white rounded-lg shadow-lg w-full max-w-lg mx-2 p-4">
         <div className="flex justify-between items-center border-b pb-2">
           <h2 className="text-xl font-semibold">{title}</h2>
