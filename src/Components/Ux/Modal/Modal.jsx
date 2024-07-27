92ea9a7c913dfc0c198ffe9361f26fe72a29d4df
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-27 12:24:09 -0300

    Feat: animation display modal

--- src/Components/Ux/Modal/Modal.jsx
+++ src/Components/Ux/Modal/Modal.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { IoMdClose } from 'react-icons/io'
+import './style.css'
 
 export default function Modal({ isOpen, onClose, title, children }) {
   if (!isOpen) return null
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className=" is-open fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg w-full max-w-lg mx-2 p-4">
         <div className="flex justify-between items-center border-b pb-2">
           <h2 className="text-xl font-semibold">{title}</h2>
